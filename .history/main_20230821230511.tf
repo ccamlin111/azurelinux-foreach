43f2c1cfa6f5339e9e4e9d763869ed57e3c502e1
Chet Camlin <ccamlin111@gmail.com>
2024-03-05 17:52:47 -0500

    Initial commit

--- .history/main_20230821230511.tf
+++ .history/main_20230821230511.tf
@@ -43,7 +43,7 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
-//standard subnet
+//standard subne
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
